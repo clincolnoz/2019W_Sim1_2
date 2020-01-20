fb4ae1c9e3a3523dbcfd5a6205f4d412b81c81c4
clincolnoz <clincolnoz@gmail.com>
2020-01-20 22:19:39 +0100

    added the call to extract audio moved it from main

---
+++ src/call_extract_audio.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from src.extractAudioFeatures import *
+
+# Extract Audio Features and save to csv
+files = ['data/external/Muppets-02-01-01.avi','data/external/Muppets-02-04-04.avi','data/external/Muppets-03-04-03.avi']
+save_audio_feature_to_csv(files)
+# files = ['data/external/Muppets-02-01-01.avi']
+# plot_features(files)
