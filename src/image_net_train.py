de46215c524decd481b36e71c66b58ab54d582ea
clincolnoz <clincolnoz@gmail.com>
2020-01-20 13:52:34 +0100

    image_net_train

--- src/image_net_train.py
+++ src/image_net_train.py
@@ -11,9 +11,11 @@ print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices("
 from tf_hub import TFHub
 from image_net_util import *
 
+# Creates a new instance and trainson un augmented images
 np.random.seed(1)
 config_file = 'ResNet50V2_0.1.yaml'
 best_model_file = setup_model_and_train(config_file)
 
+# Loads teh model from above and continues traingin with augmented images
 config_file = 'ResNet50V2_0.2.yaml'
 best_model_file = load_model_and_train(config_file,best_model_file)
