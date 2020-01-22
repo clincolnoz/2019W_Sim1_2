0fcae3eb7ae3081556c1adadfc54c13ab3f0af22
Gent Rexha <gent.rexha@gmail.com>
2020-01-22 14:11:10 +0100

    Create main.bat

---
+++ main.bat
@@ -0,0 +1,7 @@
+python src\\extract_video.py
+python src\\extractAudioFeatures.py
+python src\\label_data.py
+python src\\train_test_split.py
+python src\\image_net_train.py
+python src\\audio_net_train.py
+python src\\combined_model.py
